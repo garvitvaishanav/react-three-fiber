3f6aeee11836769d73b570f62cda43a7b029d742
Garvit Vaishnav <garvitvaishnav@Garvits-Mac-mini.local>
2025-04-27 20:03:05 +0530

    Add second section plane with X axis clipping

--- src/components/SectionPlanes.jsx
+++ src/components/SectionPlanes.jsx
@@ -6,12 +6,21 @@ const SectionPlanes = () => {
     const planeRef = useRef(new THREE.Plane(new THREE.Vector3(0, -1, 0), 0));
     const meshRef = useRef(null);
 
+    const planeXref = useRef(new THREE.Plane(new THREE.Vector3(-1, 0, 0), 0)); // X axis
+    const meshXRef = useRef(null);
+
     useFrame(() => {
         if (!meshRef.current) return;
         meshRef.current.updateMatrixWorld();
         const position = new THREE.Vector3();
         meshRef.current.getWorldPosition(position);
         planeRef.current.constant = position.y; // because normal is (0, -1, 0)
+
+        if (!meshXRef.current) return;
+        meshXRef.current.updateMatrixWorld();
+        const positionX = new THREE.Vector3();
+        meshXRef.current.getWorldPosition(positionX);
+        planeXref.current.constant = positionX.x; // because normal is (-1, 0, 0)
     })
 
     return (
@@ -23,9 +32,17 @@ const SectionPlanes = () => {
                 </mesh>
             </TransformControls>
 
+            {/* Transform Controls for X clipping plane */}
+            <TransformControls mode="translate">
+                <mesh ref={meshXRef} visible={false}>
+                    <boxGeometry args={[10, 10, 0.1]} />
+                    <meshStandardMaterial color="green" />
+                </mesh>
+            </TransformControls>
+
             <mesh>
                 <torusGeometry args={[1, 0.5, 16, 60]} />
-                <meshStandardMaterial color="blue" clippingPlanes={[planeRef.current]} />
+                <meshStandardMaterial color="blue" clippingPlanes={[planeRef.current, planeXref.current]} />
             </mesh>
         </>
     );
